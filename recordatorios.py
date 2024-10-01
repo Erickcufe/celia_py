fc49ec86ba642de226bc571e2bb5e4aebe1de32e
Erickcufe <erickcufe94@gmail.com>
2024-10-01 17:12:41 -0600

    cambios en test

---
+++ recordatorios.py
@@ -0,0 +1,19 @@
+# Funciones
+
+def saludar(nombre):
+    return f"Hola {nombre}"
+
+print(saludar("Celia"))
+
+# Listas
+numeros = [1, 2, 3, 4, 5]
+for numero in numeros:
+    print(numero)
+
+# Diccionarios
+persona = {
+    "nombre": "Celia",
+    "edad": 25
+}
+print(persona["nombre"])
+
