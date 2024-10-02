c97356ed2ede0d1f01094b65f48efccfd236a440
Erickcufe <erickcufe94@gmail.com>
2024-10-01 18:04:43 -0600

    .

--- recordatorios.py
+++ recordatorios.py
@@ -17,3 +17,15 @@ persona = {
 }
 print(persona["nombre"])
 
+
+# Clases y programación Orientada a Objetos
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def saludar(self):
+        return f"Hola, me llamo {self.nombre} y tengo {self.edad} años."
+
+persona1 = Persona("Alice", 25)
+print(persona1.saludar())
